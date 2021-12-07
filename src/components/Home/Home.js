2947696251585f00ea83bf711bcec74ba4b5da69
brian99na <brian99na@gmail.com>
2021-12-06 23:35:44 -0800

    added landing page words and buttons to scroll to main section. Also added first project slide

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './home.css'
+import { AiOutlineArrowDown } from 'react-icons/ai'
+import btc from '../../images/coinflip_img.png'
 
 function Home() {
+
+    const [arrowClick, setArrowClick] = useState(false)
+
+    const arrowRef = useRef()
+
+    const handleArrowClick = () => {
+        setArrowClick(!arrowClick)
+        setTimeout(() => {
+            !arrowClick && arrowRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+                inline: 'center'
+            })
+          }, 1100);
+        setArrowClick(!arrowClick)
+
+    }
+
     return (
         <div className='home-container'>
             <section className='home-1'>
-                <div>
+                <div className='home-1-text'>
                     <h1>Hi, I'm Brian.</h1>
                     <p>I'm a Fullstack Web Developer with an interest in design. Check out some of my work below.</p>
-                    <button>arrow</button>
+                    <AiOutlineArrowDown className={`icons ${arrowClick ? 'icon-click' : ''}`} onClick={handleArrowClick}/>
+                    <AiOutlineArrowDown className={`icons-1 ${arrowClick ? 'icon-click-1' : ''}`} onClick={handleArrowClick}/>
+                    <AiOutlineArrowDown className={`icons-2 ${arrowClick ? 'icon-click-2' : ''}`} onClick={handleArrowClick}/>
+                    <AiOutlineArrowDown className={`icons-3 ${arrowClick ? 'icon-click-3' : ''}`} onClick={handleArrowClick}/>
                 </div>
                 <div className=''>
 
                 </div>
             </section>
-
+            <section className='home-2' ref={arrowRef}>
+                <div>
+                    <h1>Projects</h1>
+                </div>
+                <div className='project-1'>
+                    <div className='project-1-title'>
+                        <h1>What If?</h1>
+                        <p>Cryptocurrency Calculator</p>
+                    </div>
+                    <img className='btc-img' src={btc} alt='btc'/>
+                </div>
+            </section>
         </div>
     )
 }
