46991942f37ccaf9533be736a007837d8629b099
Brian na <brian99na@gmail.com>
2024-11-12 23:53:40 -0800

    update experience, resume, interactive animation for 1368p and higher

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import  './about.css'
-import boatImg from '../../images/meonboat_00000.png'
 import blenderVideos from '../../videos/blender-videos_VP9.webm'
 import tags from '../../images/tags_00000.png'
+import dbx from '../../images/dbx.png'
+
 import { useNavigate } from 'react-router-dom'
 
 function About(props) {
@@ -33,43 +34,51 @@ function About(props) {
             <div className={`about-main ${pageLoad ? 'main-load' : ''}`}>
                 <section className='about'>
                     <section className='about-1'>
-                        <a href='https://www.behance.net/gallery/97734193/UCI-Rowing' target='_blank' rel='noreferrer'>
-                            <img src={boatImg} alt='boat-img' className='boat-img'/>
-                        </a>
-                        <div className='about-1-text'>
-                            <h1>My background.</h1>
-                            <p>
-                                I've spent the past couple of years in all areas of media creation.
-                                From social media videos, short films, and 2D/3D motion graphics, I've built
-                                up skills in crafting creative visuals that are engaging and appealing.
-                                Through this, I've gained valuable experience with working in collaborative settings
-                                such as film sets and media organizations at my university.
-                            </p>
+                        <div className='about-1-container'>
+                            <video playsInline autostart autoPlay loop controls={false} src={blenderVideos} className='blender-video' type="video/webm" muted/>
+                            <div className='about-1-text'>
+                                <h1>My background.</h1>
+                                <p>
+                                    I've spent the majority of my college career within all areas of media creation.
+                                    From social media videos, short films, and 2D/3D motion graphics, I've built
+                                    up skills in crafting creative visuals that are engaging and appealing.
+                                    Through this, I've gained valuable experience with working in collaborative settings
+                                    such as film sets and media organizations at my university. This has helped me tremendously
+                                    in all areas of software development. Whether it be maintaining an eye for detail or collaborating with
+                                    cross functional partners, my experience and skills have translated well into the world of software engineering.
+                                </p>
+                            </div>
                         </div>
                     </section>
                     <section className='about-2'>
-                        <video playsInline autostart autoPlay loop controls={false} src={blenderVideos} className='blender-video' type="video/webm" muted playsInline/>
-                        <div className='about-2-text'>
-                            <h1>Where I'm at now.</h1>
-                            <p>
-                                I still love to create small clips using after effects or blender in my free time, whenever I do have free time that is.
-                                I've found a new passion, software development! I recently finished the project I've been wanting to create, a creative social
-                                media style portfolio website. The first time I touched code was when I wanted to create a portfolio website where my
-                                artworks could live, and now it feels like I've come full circle. I have also now finished my software engineering bootcamp 
-                                that was extremely rigorous, but even more rewarding. I feel like I have gained so much experience on all fronts, and I feel confident in my future as a developer.
-                            </p>
+                        <div className='about-2-container'>
+                            <div className='logo-img-container'>
+                                <img className='dbx' src={dbx} alt='f'/>
+                            </div>
+                            <div className='about-2-text'>
+                                <h1>Where I'm currently at</h1>
+                                <p>
+                                    I've been working as a software engineer for the past two and a half years under the company Dropbox. Here I've learned
+                                    everything there is to know about being an engineer which includes so much more than just coding. My journey with Dropbox has ended as of November 2024,
+                                    but the road to get here included 4 different teams, 3 projects lead, a brand new onboarding experience launched, and so much more that I can be proud of.
+                                </p>
+                            </div>
                         </div>
                     </section>
                     <section className='about-3'>
-                        <img className='tags' src={tags} alt=''/>
-                        <div className='about-3-text'>
-                            <h1>Where I want to be.</h1>
-                            <p>
-                                I want to continue learning in a junior role in a professional environment.
-                                Over the next few months, I think I will try to dedicate a lot more time into refining my projects and learning more about the
-                                fundamentals of computer science. I'm also really excited to try new technologies and I'm interested in the Web3 space and all
-                                it has to offer. If you made it here, thanks for reading. I'll be updating this section as my career progress, so see you around :)
-                            </p>
+                        <div className='about-3-container'>
+                            <div className='logo-img-container'>
+                                <img className='tags' src={tags} alt=''/>
+                            </div>
+                            <div className='about-3-text'>
+                                <h1>Where I want to be.</h1>
+                                <p>
+                                    I want to continue learning in a role that will further challenge and develop my skills as an engineer. My goals are to take on impactful projects
+                                    and have a larger role in the company that I join. Through this, I will be able to elevate both myself and the people around me, something that I find valuable
+                                    when working within a team. I know that my future team and company will be something that I'm passionate about, so if you're reading this, know that I am truly committed to the product that I'll be working on. 
+                                    That's it for now, I'll be looking for any new opportunities that arise, feel free to reach out! 
+                                </p>
+                            </div>
                         </div>
                     </section>
                     <section className='about-footer'>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,23 +4,26 @@ import './home.css'
 import { AiOutlineArrowDown } from 'react-icons/ai'
 import ProjectSlide from './ProjectSlide'
 import Skills from './Skills'
-import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3, IoLogoPython } from 'react-icons/io'
-import { SiExpress, SiMongodb, SiJquery, SiDjango, SiReact } from 'react-icons/si'
+import { IoLogoHtml5, IoLogoCss3, IoLogoPython, IoIosArrowDropdown } from 'react-icons/io'
+import { SiExpress, SiMongodb, SiJquery, SiDjango, SiReact, SiTypescript } from 'react-icons/si'
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 import btc from '../../images/coinflip_img.png'
 import createArt from '../../images/android-chrome-512x512-removebg-preview.png'
 import madlibArt from '../../images/madlibs_00000.png'
 import homepageArt from '../../images/goodmorning_00000.png'
+import dbx from '../../images/dbx.png'
 
 function Home(props) {
     const [arrowClick, setArrowClick] = useState(false)
+    const [experienceOneOpen, setExperienceOneOpen] = useState(false)
+    const [artClick, setArtClick] = useState(false)
     const navigate = useNavigate()
     const projectsRef = useRef()
     const skillsRef = useRef()
     const contactRef = useRef()
 
     const skills = [
-        {img: 'HTML5', name: <IoLogoHtml5/>}, {img: 'CSS3', name: <IoLogoCss3/>}, {img: 'JavaScript', name: <IoLogoJavascript/>}, 
+        {img: 'HTML5', name: <IoLogoHtml5/>}, {img: 'CSS3', name: <IoLogoCss3/>}, {img: 'TypeScript', name: <SiTypescript/>}, 
         {img: 'jQuery', name: <SiJquery/>}, {img: 'React', name: <SiReact/>}, {img: 'Express', name: <SiExpress/>}, 
         {img: 'MongoDB', name: <SiMongodb/>}, {img: 'Python', name: <IoLogoPython/>}, {img: 'Django', name: <SiDjango/>}
     ]
@@ -68,6 +71,17 @@ function Home(props) {
         alert("I've received your message, thank you!")
     }
 
+    const handleExperienceDropdown = () => {
+        setExperienceOneOpen((prevVal) => !prevVal)
+    }
+
+    const handleArtClick = () => {
+        setArtClick(true)    
+        setTimeout(() => {
+            setArtClick(false)
+        }, 6000)
+    }
+
     useEffect(() => {
         if (!props.contactHeader && !props.projectHeader) {
             window.scrollTo(0,0)
@@ -93,6 +107,16 @@ function Home(props) {
         )
     })
 
+    const saveCursorPosition = function(x, y) {
+        pos.x = (x / window.innerWidth).toFixed(2);
+        pos.y = (y / window.innerHeight).toFixed(2);
+        document.documentElement.style.setProperty('--x', pos.x);
+        document.documentElement.style.setProperty('--y', pos.y);
+    }
+    
+    const pos = { x : 0, y : 0 };
+    document.addEventListener('mousemove', e => { saveCursorPosition(e.clientX, e.clientY); })
+    
     return (
         <div className={`home-container ${props.pageLeave ? 'page-leave': ''}`}>
             <section className='home-1'>
@@ -109,18 +133,56 @@ function Home(props) {
                         </Link>
                     </div>
                 </div>
-                <div className={`home-1-art ${arrowClick ? 'art-click' : ''}`}>
-                    <div className='head'>
-                        <div className='hat-main'>
+                <div className={`home-1-art`}>
+                    <div className={`head ${artClick ? 'head-art-click' : ''}`} onClick={handleArtClick}>
+                        <div className={`hat-main ${artClick ? 'hat-art-click' : ''}`}>
                             <div className='hat-top'></div>
                             <div className='hat-bot'></div>
                         </div>
-                        <div className='eye1'>
-                            <div className='pupil'></div>
+                        <div className={`eye1 ${artClick ? 'eye1-art-click' : ''}`}>
+                            <div className={`pupil ${artClick ? 'pupil-art-click' : ''}`}></div>
                         </div>
-                        <div className='eye2'>
-                            <div className='pupil'></div>
+                        <div className={`eye2 ${artClick ? 'eye2-art-click' : ''}`}>
+                            <div className={`pupil ${artClick ? 'pupil-art-click' : ''}`}></div>
                         </div>
+                    </div>
+                </div>
+            </section>
+            <section className='home-25' >
+                <h1 className='home-25-title' >Experience</h1>
+                <div className='experience-1'>
+                    <div className='experience-1-header'>
+                        <div className='experience-1-chip-header-container'>
+                            <div className='dbx-container'>
+                                <img className='dbx' src={dbx} alt='f'/>
+                                <h2>Dropbox</h2>
+                            </div>
+                            <h4 className='chip'>April'22 - November'24</h4>
+                        </div>
+                        <IoIosArrowDropdown onClick={handleExperienceDropdown} className='experience-dropdown-icon'/> 
+                    </div>
+                    <div className={`experience-1-dropdown ${experienceOneOpen ? 'experience-dropdown-open' : ''}`}>
+                        <p><strong>• Lead engineering efforts</strong> in improving desktop app usability and address customer concerns surrounding the Selective Sync desktop app feature. Managed complete process of collaborating w/ design, code implementation, metrics, and feature rollout. Reduced customer experience tickets related to Selective Sync by 60% over the span of 2 months.
+                        <br /><br /><strong>• Lead engineering efforts</strong> for a revamp to the Desktop App’s Tray design which aimed at improving engagement and retention of users on the Desktop App. Handled code implementation, metrics, and rollout. Redesign focused on the increased exposure of the sharing and search features, both of which saw increase of activity by 141% and 41% respectively.
+                        <br /><br /><strong>• Conducted a variety of experiments</strong> on multiple Dropbox products including Backup, FSS, and Sync. Took ownership of complete process of feature gating, implementation, logging, monitoring metrics, A/B testing, rollout, and GA of feature.
+                        <br /><br /><strong>• Launched a brand new onboarding experience</strong> for the Desktop App, migrating from legacy ‘local views’ framework to the Electron framework on a new typescript based architecture.
+                        <br /><br /><strong>• Worked on 3 different engineering teams</strong>, having a DRI role in the following projects:
+                        <br /><strong>• Backup Product (6 months):</strong> Web and Desktop Code, Front End Backup Product Related Features (DRI for Computer Backup Rename Feature & External Drive Backup Rename Feature)
+                        <br /><strong>• Desktop Discovery (1 year):</strong> Web and Desktop Code, Full Stack Desktop Onboarding Product Related Features (DRI for  Top of Funnel Web to Desktop Conversion Experiment, Co-DRI for Video Onboarding Experiment)
+                        <br /><strong>• Desktop Foundation (6 months):</strong> Desktop Code, Full Stack Desktop Foundation Product Related Features (DRI for Selective Sync Education Experiment, Tray 2024 Redesign, & Sync Errors 2024 Redesign)
+                        </p>
+                    </div>
+                </div>
+                <div className='experience-2'>
+                    <div className='experience-2-header'>
+                        <div className='experience-2-chip-header-container'>
+                            <div className='loading-container'>
+                                <div className='loading-skeleton'></div>
+                                <h2>...Pending</h2>
+                            </div>
+                            <h4 className='chip'>Current - ∞</h4>
+                        </div>
+                        <IoIosArrowDropdown className='experience-dropdown-icon-disabled'/> 
                     </div>
                 </div>
             </section>
